c9e473bdfbfbd4586fcb825f692aa773303b2893
username <email@git.com>
2022-01-07 12:58:03 +0200

    added check for ubus timeout

--- datasender/src/main.c
+++ datasender/src/main.c
@@ -14,31 +14,34 @@ int main(int argc, char *argv[])
   IoTPConfig *config = NULL;
   IoTPDevice *device = NULL;
 
-/*Create IOTPConfig*/
+  /*Create IOTPConfig*/
   rc = IoTPConfig_create(&config, NULL);
-  if(rc != IOTPRC_SUCCESS) {
+  if (rc != IOTPRC_SUCCESS)
+  {
     fprintf(stderr, "ERROR: Failed to create IOTPConfig\n");
     end_comms(device, config);
     return -1;
   }
 
-/*Configure IOTPConfig*/
+  /*Configure IOTPConfig*/
   rc = config_comms(config, argc, argv);
-  if(rc != IOTPRC_SUCCESS) {
+  if (rc != IOTPRC_SUCCESS)
+  {
     fprintf(stderr, "ERROR: Failed to configure comms\n");
     end_comms(device, config);
     return -1;
   }
 
-/*Create IOTPConfig*/
+  /*Create IOTPConfig*/
   rc = IoTPDevice_create(&device, config);
-  if(rc != IOTPRC_SUCCESS) {
+  if (rc != IOTPRC_SUCCESS)
+  {
     fprintf(stderr, "ERROR: Failed to create IOTPDevice\n");
     end_comms(device, config);
     return -1;
   }
 
-/* Setup other things for communication with IBM cloud */
+  /* Setup other things for communication with IBM cloud */
   rc = setup_comms(device);
   if (rc != IOTPRC_SUCCESS)
   {
@@ -54,7 +57,7 @@ int main(int argc, char *argv[])
 
   /* Setup connection to ubus */
   struct ubus_context *ctx = ubus_connect(NULL);
-  if (!ctx) 
+  if (!ctx)
   {
     fprintf(stderr, "ERROR: Failed to connect to ubus\n");
     end_comms(device, config);
@@ -62,7 +65,7 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  loop(ctx, device);  
+  loop(ctx, device);
 
   end_comms(device, config);
   ubus_free(ctx);
@@ -71,29 +74,31 @@ int main(int argc, char *argv[])
 }
 
 /* loop gets memory info from ubus to array mem and sends it to Watson IoT Platform. */
-void loop(struct ubus_context *ctx, struct IoTPDevice *device) {
+void loop(struct ubus_context *ctx, struct IoTPDevice *device)
+{
   uint32_t id;
   uint64_t mem[4] = {0, 0, 0, 0};
+  int rc1, rc2;
 
   while (deamonize != 0)
   {
-    if (ubus_lookup_id(ctx, "system", &id) ||
-        ubus_invoke(ctx, id, "info", NULL, board_cb, mem, 3000))
+    if ((rc1 = ubus_lookup_id(ctx, "system", &id)) ||
+        (rc2 = ubus_invoke(ctx, id, "info", NULL, board_cb, mem, 3000)))
     {
-
-      fprintf(stderr, "WARN: Cannot request memory info from procd\n");
-      fprintf(stdout, "INFO: Maybe reconnecting will help? Reconnecting to ubus...\n");
-      ctx = ubus_connect(NULL);
-      if (!ctx)
+      if (rc1 == UBUS_STATUS_TIMEOUT || rc2 == UBUS_STATUS_TIMEOUT)
       {
-        fprintf(stdout, "Reconnecting didn't work. Exiting.\n");
-        return;
+        fprintf(stderr, "WARN: Cannot request memory info from procd as ubus has timed out. Reconnecting...\n");
+        ctx = ubus_connect(NULL);
+        if (!ctx)
+        {
+          fprintf(stdout, "ERROR: Could not reconnect to ubus. Exiting.\n");
+          return;
+        }
       }
     }
-    
+
     comms_send_message(device, mem);
     memset(mem, 0, sizeof(int) * 4);
     sleep(5);
   }
-
 }
